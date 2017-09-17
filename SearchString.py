d08159f5319c66496cd7156fe27c16e9ffabf6c2
Raveendra Seetharam <ravibhattinkw@gmail.com>
2017-09-17 18:47:00 -0400

    Update SearchString.py
    Added capability to store and return lines containing string

--- SearchString.py
+++ SearchString.py
@@ -4,14 +4,20 @@ import time
 
 def SearchString(filename,str):
    count = 0
+   lines = []
    t = time.time()
    FileHandle = open(filename, 'r')
    for line in FileHandle:
       if str in line:
+        lines.append(line)
         count += 1
    t = time.time() - t 
-   print "Found %d times in %f seconds" % (count,t)
+   print ("Found %d times in %f seconds" % (count,t))
+   return lines,count
 
 filename = 'Out.rtf'
-str = "Blackberry"
-SearchString(filename,str)
+str = 'Blackberry'
+lines,count = SearchString(filename,str)
+print ("after SearchString count=",count)
+for i in range(0,count):
+    print ("lines = \n",lines[i])
