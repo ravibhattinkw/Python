1de597c8b5e9fb2d47e7bb606abdf5a3c1eac244
Raveendra Seetharam <ravibhattinkw@gmail.com>
2017-09-26 12:09:26 -0400

    Update Patient.py
    working PatientEnrol and display code

--- Patient.py
+++ Patient.py
@@ -1,44 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 09:06:57 2017
+
+@author: raveendra.seetharam
+"""
+
 #!/usr/bin/python
 
 import time
 import datetime
 
-class Patient:
- "A Patient Record Class"
+patientId = 1
 
+class Patient:
+ "Base Patient Record Class"
  
- #def __init__(self,FirstName,LastName,TelNum,birthdate,disease,deceased):
- def __init__(self,FirstName,profession,LastName):
-    "Constructor"
+ def __init__(self,FirstName,LastName,profession,TelNum,Disease,Deceased,Birthdate):
+    "Constructor for creating patient record"
     self.firstname = FirstName
-    self.profession = profession
     self.lastname = LastName
-    #self.birthdate = birthdate
-    #self.disease = disease
-    #self.deceased = deceased
-    #self.telnum = TelNum
-    #self.time = time()
+    self.profession = profession
+    self.telnum = TelNum
+    self.disease = Disease
+    self.deceased = Deceased
+    self.birthdate = Birthdate
     self.patientId = patientId
     self.patientId += 1
+
+ def getMaxPatientCount(self):
+    return self.patientId
+ 
+def calculate_age(birthdate):
+     birthyear = birthdate.split(',')
+     currentyear = datetime.datetime.today().date()
+     #print ("birthyear,currentyear",int(birthyear[0]),currentyear.year)
+     age = currentyear.year - int(birthyear[0])
+     return age
     
-    
- def displayPatient(self,PatientId):   
-     if (PatientDict['PatientId'] == PatientId):
-        print ("Name :", PatientDict['name'])
-        #print ("Age  :", PatientDict['age'])
-        #print ("disease :",PatientDict['disease'])
+def displayPatientDetails(TelNum):
+     for p in PatientList:
+         if(p.telnum == TelNum):
+            print ("Name :", p.firstname, p.lastname)
+            print ("profession :", p.profession)
+            print ("Disease :", p.disease)
+          
+            age = calculate_age(p.birthdate)
+            print ("Age =",age)
+            
+            if (p.deceased == 1):
+                print ("Patient deceased")
+            else:
+                print ("Patient Alive")
+            
 
+if __name__ == "__main__":
+  PatientList = []
+  patient = Patient("Raj","kundra","PA","1234","Fever","0","1975,7,23")
+  PatientList.append(patient)
 
-#def enrolPatient(FirstName,LastName,TelNum,birthdate,disease,deceased):
-#    for i=0 in range(5):
-#        Patientlist.append({Patient(FirstName,LastName,TelNum,birthdate,disease,deceased)})
-
-patientId = 1
-#Patientlist = [dict() for x in range(5)]
-#patient = Patient("Raj")
-#print(patient.firstname)
-#Patientlist.append({patient})
-#print Patientlist
-people = [Patient("Nick", "Programmer","carter"), Patient("Alice","Engineer","johnson")]
-Patientlist = dict([ (p.firstname, p.profession, p.lastname) for p in people ])
-print Patientlist
+  patient1 = Patient("Ravi","Ram","CEO","1235","cold","0","1984,3,12")
+  PatientList.append(patient1)
+  print (PatientList)
+  count = patient.getMaxPatientCount()
+  print ("Num of Patients=",count)
+  
+  displayPatientDetails("1235")
